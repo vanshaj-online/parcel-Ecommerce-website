4a0d270f04c637df585677d33377a93bbccc3b91
vanshaj-online <singhvanshaj09@gmail.com>
2024-02-09 17:16:03 +0530

    made navbar,searchbar and footer responsive in women.html

--- women.js
+++ women.js
@@ -3,7 +3,6 @@ function scroll(){
     const prev = document.querySelector('.prev');
     const next = document.querySelector('.next');
     let lastelem = document.querySelector('#last');
-    let check = 'hidden';
     const observer = new IntersectionObserver(entries => {
         entries.forEach(last => {
             if (last.intersectionRatio !== 1) {
@@ -44,4 +43,25 @@ function dropdownprofile() {
     })
 }
 dropdownprofile()
-console.log(window.innerWidth)
+
+function toggleMenu() {
+    let openmenu = document.querySelector('[data-open-menu]')
+    let closemenu = document.querySelector('[data-close-menu]')
+    let menu = document.querySelector('.side-bar')
+    let nav = document.querySelector('nav')
+    
+
+    openmenu.addEventListener('click', function () {
+        menu.style.transform = 'translatex(0%)'
+        main.style.opacity = '0.5'
+        nav.style.opacity = '0.5'
+    })
+    closemenu.addEventListener('click', function () {
+        menu.style.transform = 'translatex(100%)'
+        nav.style.opacity = '1'
+        main.style.opacity = '1'
+    })
+
+}
+
+toggleMenu()
